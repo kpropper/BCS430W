a6cba3f9439cbe8b9bcc6f0678878966a1079f7b
Ellishiel <32434770+Ellishiel@users.noreply.github.com>
2017-11-06 15:06:18 -0500

    test
    test

--- www/getdata.php
+++ www/getdata.php
@@ -7,11 +7,13 @@ if(!empty($_POST["CategoryID"])) {
   $query = "SELECT DISTINCT  ManufacturerName FROM manufacturer m JOIN assetmodel a ON m.ManufacturerID = a.ManufacturerID WHERE a.CategoryID = $cid";
 
   $results = mysqli_query($mysqli, $query);
+  ?><option value="">Select Manufacturer</option><?php
     foreach ($results as $manufacturer) {
       ?>
     <option value="<?php echo $manufacturer["ManufacturerName"];?>"><?php echo $manufacturer["ManufacturerName"]?> </option>
     <?php
        }
   }
+
 
 ?>
