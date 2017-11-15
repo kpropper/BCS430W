b581156c7423f8da8760950c8dcac46c3e4f3928
kpropper <kpropper@gmail.com>
2017-11-14 19:15:08 -0500

    Code Cleanup
    Just Cleaned Up some code

--- www/registerUser.php
+++ www/registerUser.php
@@ -17,9 +17,6 @@
 
 	if ($msg == NULL) 
 	{
-		//$mysqli = new mysqli('localhost', 'root', '', 'test1');
-		//if ($mysqli->connect_error)
-		//	die('Connect Error: ' . $mysqli->connect_error);
 		include("sqlConnect.php");
 		
 		$email = $mysqli->escape_string($_POST['email']);
