ca495e6abcc92ad8b720612f222cb4990df0f024
Ellishiel <32434770+Ellishiel@users.noreply.github.com>
2017-11-16 19:51:28 -0500

    Script, ERD and Dummy Data Update
    Script, ERD and Dummy Data Update

--- SQL Scripts/test1scripts.sql
+++ SQL Scripts/test1scripts.sql
@@ -25,19 +25,19 @@ this the ID's are already assigned in the
 CSV files */
 
 CREATE TABLE AssetCategory (
-CategoryID INT NOT NULL,
+CategoryID INT unsigned NOT NULL AUTO_INCREMENT,
 CategoryName varchar (75),
 PRIMARY KEY (CategoryID)
 );
 
 CREATE TABLE Manufacturer (
-ManufacturerID INT NOT NULL,
+ManufacturerID INT unsigned NOT NULL AUTO_INCREMENT,
 ManufacturerName varchar (100),
 PRIMARY KEY (ManufacturerID)
 );
 
 CREATE TABLE AssetModel (
-ModelID INT NOT NULL,
+ModelID INT unsigned NOT NULL AUTO_INCREMENT,
 CategoryID INT NOT NULL,
 ManufacturerID INT NOT NULL,
 ModelName varchar (100) NOT NULL,
@@ -49,7 +49,7 @@ FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID)
 );
 
 CREATE TABLE Processor (
-ProcessorID INT NOT NULL,
+ProcessorID INT unsigned NOT NULL AUTO_INCREMENT,
 ProcessorType varchar (50) NOT NULL,
 ProcessorSpeed double NOT NULL,
 ProcessorQty INT NOT NULL,
@@ -59,7 +59,7 @@ PRIMARY KEY (ProcessorID)
 
 
 CREATE TABLE Memory (
-MemoryID INT NOT NULL,
+MemoryID INT unsigned NOT NULL AUTO_INCREMENT,
 MemoryType varchar (25) NOT NULL,
 MemorySize varchar (10) NOT NULL,
 MemoryQty INT NOT NULL,
@@ -68,7 +68,7 @@ PRIMARY KEY (MemoryID)
 );
 
 CREATE TABLE HardDrive (
-HardDriveID INT NOT NULL,
+HardDriveID INT unsigned NOT NULL AUTO_INCREMENT,
 HardDriveType varchar (25) NOT NULL,
 HardDriveSize varchar (25) NULL,
 HardDriveQty INT NULL,
@@ -76,34 +76,53 @@ HardDriveValue double NULL,
 PRIMARY KEY (HardDriveID)
 );
 
+CREATE TABLE Inventory (
+InventoryID INT unsigned NOT NULL AUTO_INCREMENT,
+UserID INT NOT NULL,
+StatusID INT NOT NULL,
+Inventory_Value double Null,
+InitQuoteMin double NULL,
+InitQuoteMax double NULL,
+FinalQuote double NULL,
+PRIMARY KEY (InventoryID, UserID, StatusID),
+FOREIGN KEY (UserID) REFERENCES User(UserID),
+FOREIGN KEY (StatusID) REFERENCES Status(StatusID)
+);
+
+CREATE TABLE Status (
+StatusID INT unsigned NOT NULL AUTO_INCREMENT,
+InventoryID INT NOT NULL,
+StatusName varchar (50) not null,
+StatusDate date null,
+QuoteValue double null,
+StatusMessage varchar (500) null,
+PRIMARY KEY (StatusID),
+FOREIGN KEY (InventoryID) REFERENCES Inventory(InventoryID)
+);
+
 CREATE TABLE Asset (
-AssetID INT NOT NULL,
+AssetID INT unsigned NOT NULL AUTO_INCREMENT,
+InventoryID INT NOT NULL,
 UserID INT NOT NULL,
 ModelID INT NOT NULL,
 HardDriveID INT NOT NULL,
 ProcessorID INT NOT NULL,
 MemoryID INT NOT NULL,
-InventoryID INT NOT NULL,
 SerialNumber varchar (100) NULL,
 ProductNumber varchar (100) NULL,
 Quantity INT NOT NULL,
+CustomerConditionMod double null,
+ActualConditionMiod double null,
 AssetValue double NULL,
 PRIMARY KEY (AssetID),
-FOREIGN KEY (UserID) REFERENCES User(UserID),
+FOREIGN KEY (InventoryID) REFERENCES Inventory(InventoryID),
 FOREIGN KEY (ModelID) REFERENCES AssetModel(ModelID),
 FOREIGN KEY (HardDriveID) REFERENCES HardDrive(HardDriveID),
 FOREIGN KEY (ProcessorID) REFERENCES Processor(ProcessorID),
-FOREIGN KEY (MemoryID) REFERENCES Memory(MemoryID),
-FOREIGN KEY (InventoryID) REFERENCES Inventory(InventoryID)
+FOREIGN KEY (MemoryID) REFERENCES Memory(MemoryID)
 );
 
-CREATE TABLE Inventory (
-InventoryID INT unsigned NOT NULL AUTO_INCREMENT,
-UserID INT NOT NULL,
-Inventory_Value double Null,
-PRIMARY KEY (InventoryID, UserID),
-FOREIGN KEY (UserID) REFERENCES User(UserID)
-);
+
 
 /*input data for the tables */
 
